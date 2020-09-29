969db52a5d6a81dde6facaf3540f33d7dc6160c7
Piscinao <henhvj@gmail.com>
2020-09-28 23:30:16 -0300

    should create tags when new transactions, should not create tags when they already exists

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,7 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import Category from '../models/Category';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
@@ -20,11 +22,33 @@ class CreateTransactionService {
   }: Request): Promise<Transaction> {
     // Instanciar repositório
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    // Verifica se a categoria já existe
+
+    // Bsuca do db e usa o id retornado
+
+    // Não existe então cria
 
     const transaction = transactionsRepository.create({
       title,
       value,
       type,
+      category: transactionCategory,
     });
 
     await transactionsRepository.save(transaction);
